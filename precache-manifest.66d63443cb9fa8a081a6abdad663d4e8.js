0cb90a9ecb823804de07c97fa3a3fcaacd87ffa6
William Fligor <williamtfligor@gmail.com>
2020-04-19 14:22:37 -0400

    Updates

---
+++ precache-manifest.66d63443cb9fa8a081a6abdad663d4e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19dac9e0a051bc73ecb89fca61c99653",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca70d0b3916229949425",
+    "url": "/static/css/main.f9eb2477.chunk.css"
+  },
+  {
+    "revision": "30f78505c168765bd54f",
+    "url": "/static/js/2.e5a358c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e5a358c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca70d0b3916229949425",
+    "url": "/static/js/main.f2e3a98b.chunk.js"
+  },
+  {
+    "revision": "d14255b45132253ec0ce",
+    "url": "/static/js/runtime-main.b37cd48c.js"
+  }
+]);

1958661ad8ff1c301f54d5d2ae3c08f74135fe0b
William Fligor <williamtfligor@gmail.com>
2020-04-19 14:15:06 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import fortune from "fortune-teller";
+import cowsay from "cowsay2";
+
+function speak(text) {
+    speechSynthesis.cancel();
+
+    var voices = speechSynthesis.getVoices();
+    var msg = new SpeechSynthesisUtterance();
+    msg.text = text;
+
+    var r = Math.floor(Math.random() * voices.length + 1);
+    msg.voice = voices[r];
+
+    speechSynthesis.speak(msg);
+}
+
+function getCow() {
+    var fort = fortune.fortune();
+    fort = cowsay.say(fort, {
+        face: "parrot",
+        e: "oO",
+        T: "U",
+    });
+
+    var s = fort.replace(/[^-a-z0-9 ]/gi, "");
+    fort = fort.replace(/ /g, "&emsp;");
+    fort = fort.replace(/\n/g, "<br />");
+
+    return {
+        body: {
+            __html: fort,
+        },
+        text: s,
+    };
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { body, text } = getCow();
+
+    return (
+        <div className="App" onClick={() => speak(text)}>
+            <header className="App-header">
+                <div dangerouslySetInnerHTML={body}></div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
